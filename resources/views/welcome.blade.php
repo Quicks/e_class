e40b143ba3827339e8166bfc00c366aa1b42e5b8
Julija <you@example.com>
2016-10-03 20:42:59 +0300

    add bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,43 +3,89 @@
     <head>
         <title>Laravel</title>
 
+
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap/bootstrap.min.css') }}" />
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap/mdb.min.css') }}" />
+        <link rel="stylesheet" href="{{ URL::asset('css/bootstrap/style.css') }}" />
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">E class</div>
+    <nav class="navbar navbar-dark navbar-fixed-top scrolling-navbar">
+
+
+
+    </nav>
+    <!--/.Navbar-->
+
+    <!--Mask-->
+    <div class="view hm-black-strong">
+        <div class="full-bg-img flex-center">
+            <ul class="animated fadeInUp">
+                <li>
+                    <h1 class="h1-responsive">Електронний клас</h1></li>
+                <li>
+                    <p>Найкращий помічник вчителів, дітей та їх батьків</p>
+                </li>
+                <li>
+                    <a target="_blank" href="http://mdbootstrap.com/getting-started/" class="btn btn-primary btn-lg">Вхід</a>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!--/.Mask-->
+
+    <!--Footer-->
+    <footer class="page-footer center-on-small-only">
+
+        <!--Footer Links-->
+        <div class="container-fluid">
+            <div class="row">
+
+                <!--First column-->
+                <div class="col-md-3 offset-md-1">
+                    <h5 class="title">Про наш проект</h5>
+                    <p>Даний проект розроблений спеціалістами для покращення якості шкільної освіти. </p>
+
+                    <p>Електронний клас покращує взаємодію між вчителями, учнями та батьками.</p>
+                </div>
+                <!--/.First column-->
+
+                <hr class="hidden-md-up">
+
+
+
+                <hr class="hidden-md-up">
+
+
+                <hr class="hidden-md-up">
+
+
+
             </div>
         </div>
+        <!--/.Footer Links-->
+
+        <hr>
+
+
+        <div class="footer-copyright">
+            <div class="container-fluid">
+                © 2016  IT-Academy: <a href="http://ita.in.ua/">ita.in.ua</a>
+
+            </div>
+        </div>
+        <!--/.Copyright-->
+
+    </footer>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+                <script type="text/javascript" src="{{ URL::asset('js/bootstrap/jquery-2.2.3.min.js') }}"></script>
+                <script type="text/javascript" src="{{ URL::asset('js/bootstrap/tether.min.js') }}"></script>
+
+                <script type="text/javascript" src="{{ URL::asset('js/bootstrap/bootstrap.min.js') }}" ></script>
+                <script type="text/javascript" src="{{ URL::asset('js/bootstrap/mdb.min.js') }}" ></script>
     </body>
 </html>
