0edf1e46f7629b44fe97d2e98e18473ef037a184
Yulia <komashka.stor@gmail.com>
2016-10-08 17:20:02 +0300

    fix

---
+++ resources/views/user/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                Some User!
+                User Email -
+                User Name -
+                User
+            </div>
+        </div>
+    </div>
+@endsection
