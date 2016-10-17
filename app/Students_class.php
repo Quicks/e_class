d6247d467946b0ac6cb73034a885425fe3ec67e7
Yulia <komashka.stor@gmail.com>
2016-10-17 17:04:37 +0300

    "create relations"

--- app/Students_class.php
+++ app/Students_class.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Students_class extends Model
 {
     protected $table = 'classList';
+
+    public function teacher() {
+        return $this->hasOne('app/Teacher');
+    }
+
+    public function school()
+    {
+        return $this->belongsTo('App\Students_class');
+    }
+
 }
--- app/Teacher.php
+++ app/Teacher.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     protected $table = 'Teachers';
+
+    public function students_class() {
+
+        return $this->belongsTo('app/Students_class');
+
+    }
 }
