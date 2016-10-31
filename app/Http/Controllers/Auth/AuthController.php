18018caec260681825d7a5894a1cebd10097fae5
Yulia <komashka.stor@gmail.com>
2016-10-31 19:42:02 +0200

    fix auth

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,7 +63,6 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        dd($data);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
