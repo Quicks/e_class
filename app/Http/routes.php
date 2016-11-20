41fc3c5cfa828e54ae78e05cd1824c127a1b993d
Nazar <kushniruknazar@gmail.com>
2016-11-20 17:26:01 +0200

    access to admin route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,4 +44,8 @@ Route::get('/teacher', 'UsersController@teacher');
 //});
 
 
-//, 'middleware'=>'admin'
+Route::group( ['middleware' => 'admin'], function () {
+    Route::get('/admin','UsersController@admin');
+    //   Route::resource('roles', 'Admin\UserRolesController');
+});
+
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,128 +12,128 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//          $this->call(UsersTableSeeder::class);
-//        DB::table('users')->insert([
-//            'name' => str_random(10),
-//            'type' => str_random(3).'_type',
-//            'email' => str_random(10).'@gmail.com',
-//        ]);
-//        DB::table('users')->insert([
-//            'name' => str_random(10),
-//            'type' => str_random(3).'_type',
-//            'email' => str_random(10).'@gmail.com',
-//        ]);
-//        DB::table('users')->insert([
-//            'name' => str_random(10),
-//            'type' => str_random(3).'_type',
-//            'email' => str_random(10).'@gmail.com',
-//        ]);
-//        DB::table('users')->insert([
-//            'name' => str_random(10),
-//            'type' => str_random(3).'_type',
-//            'email' => str_random(10).'@gmail.com',
-//        ]);
-//        DB::table('users')->insert([
-//            'name' => str_random(10),
-//            'type' => str_random(3).'_type',
-//            'email' => str_random(10).'@gmail.com',
-//        ]);
-//        DB::table('users')->insert([
-//            'name' => str_random(10),
-//            'type' => str_random(3).'_type',
-//            'email' => str_random(10).'@gmail.com',
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers')->insert([
-//            'class_name' => str_random(3).'X',
-//            'name' => str_random(30).'_ПІБ',
-//            'email' => str_random(10).'@gmail.com'
-//        ]);
-//        DB::table('teachers_to_class')->insert([
-////            'teachers_id' => 1,
-////            'classes_id' => 2
-//        ]);
-//        DB::table('teachers_to_class')->insert([
-////            'teachers_id' => 3,
-////            'classes_id' => 4
-//        ]);
-//        DB::table('timetables')->insert([
-//            'day' => 'monday',
-//            'number' => 1
-//        ]);
-//        DB::table('timetables')->insert([
-//            'day' => 'tuesday',
-//            'number' => 2
-//        ]);
-//        DB::table('timetables')->insert([
-//            'day' => 'wednesday',
-//            'number' => 3
-//        ]);
-//        DB::table('timetables')->insert([
-//            'day' => "thursday",
-//            'number' => 4
-//        ]);
-//        DB::table('timetables')->insert([
-//            'day' => 'friday',
-//            'number' => 5
-//        ]);
-//        DB::table('times')->insert([
-//            'time' => '8.30 - 9.15',
-//        ]);
-//        DB::table('times')->insert([
-//            'time' => '9.25 - 10.10',
-//        ]);
-//        DB::table('times')->insert([
-//            'time' => '10.20 - 11.05',
-//        ]);
-//        DB::table('times')->insert([
-//            'time' => '11.25 - 12.10',
-//        ]);
-//        DB::table('times')->insert([
-//            'time' => '12.20 - 13.05',
-//        ]);
-//        DB::table('times')->insert([
-//            'time' => '13.15 - 14.00',
-//        ]);
+          $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'type' => str_random(3).'_type',
+            'email' => str_random(10).'@gmail.com',
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'type' => str_random(3).'_type',
+            'email' => str_random(10).'@gmail.com',
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'type' => str_random(3).'_type',
+            'email' => str_random(10).'@gmail.com',
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'type' => str_random(3).'_type',
+            'email' => str_random(10).'@gmail.com',
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'type' => str_random(3).'_type',
+            'email' => str_random(10).'@gmail.com',
+        ]);
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'type' => str_random(3).'_type',
+            'email' => str_random(10).'@gmail.com',
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers')->insert([
+            'class_name' => str_random(3).'X',
+            'name' => str_random(30).'_ПІБ',
+            'email' => str_random(10).'@gmail.com'
+        ]);
+        DB::table('teachers_to_class')->insert([
+//            'teachers_id' => 1,
+//            'classes_id' => 2
+        ]);
+        DB::table('teachers_to_class')->insert([
+//            'teachers_id' => 3,
+//            'classes_id' => 4
+        ]);
+        DB::table('timetables')->insert([
+            'day' => 'monday',
+            'number' => 1
+        ]);
+        DB::table('timetables')->insert([
+            'day' => 'tuesday',
+            'number' => 2
+        ]);
+        DB::table('timetables')->insert([
+            'day' => 'wednesday',
+            'number' => 3
+        ]);
+        DB::table('timetables')->insert([
+            'day' => "thursday",
+            'number' => 4
+        ]);
+        DB::table('timetables')->insert([
+            'day' => 'friday',
+            'number' => 5
+        ]);
+        DB::table('times')->insert([
+            'time' => '8.30 - 9.15',
+        ]);
+        DB::table('times')->insert([
+            'time' => '9.25 - 10.10',
+        ]);
+        DB::table('times')->insert([
+            'time' => '10.20 - 11.05',
+        ]);
+        DB::table('times')->insert([
+            'time' => '11.25 - 12.10',
+        ]);
+        DB::table('times')->insert([
+            'time' => '12.20 - 13.05',
+        ]);
+        DB::table('times')->insert([
+            'time' => '13.15 - 14.00',
+        ]);
 //        DB::table('subjects')->insert([
 //            'subject' => 'math',
 //        ]);
@@ -158,67 +158,67 @@ class DatabaseSeeder extends Seeder
 //        DB::table('subjects')->insert([
 //            'subject' => 'math',
 //        ]);
-//        DB::table('classrooms')->insert([
-//            'number' => 54,
-//        ]);
-//        DB::table('classrooms')->insert([
-//            'number' => 45,
-//        ]);
-//        DB::table('classrooms')->insert([
-//            'number' => 48,
-//        ]);
-//        DB::table('classrooms')->insert([
-//            'number' => 42,
-//        ]);
-//        DB::table('classrooms')->insert([
-//            'number' => 17,
-//        ]);
-//        DB::table('classrooms')->insert([
-//            'number' => 19,
-//        ]);
-//        DB::table('roles')->insert([
-//            'title' => 'Admin',
-//            'description' => 'Admin'
-//        ]);
-//        DB::table('roles')->insert([
-//            'title' => 'Teacher',
-//            'description' => 'Teacher'
-//        ]);
-//        DB::table('roles')->insert([
-//            'title' => 'Student',
-//            'description' => 'Student'
-//        ]);
-//        DB::table('user_roles')->insert([
-//            'user_id' => 1,
-//            'role_id' => 2
-//        ]);
-//        DB::table('user_roles')->insert([
-//            'user_id' => 2,
-//            'role_id' => 1
-//        ]);
-        DB::table('subjects')->insert([
-        'title' => 'Math',
-        'description' => 'Math'
-    ]);
-        DB::table('subjects')->insert([
-            'title' => 'Language',
-            'description' => 'Language'
+        DB::table('classrooms')->insert([
+            'number' => 54,
         ]);
-        DB::table('subjects')->insert([
-            'title' => 'History',
-            'description' => 'History'
+        DB::table('classrooms')->insert([
+            'number' => 45,
         ]);
-        DB::table('subjects')->insert([
-            'title' => 'Chemistry',
-            'description' => 'Chemistry'
+        DB::table('classrooms')->insert([
+            'number' => 48,
         ]);
-        DB::table('subjects')->insert([
-            'title' => 'Geography',
-            'description' => 'Geographyy'
+        DB::table('classrooms')->insert([
+            'number' => 42,
         ]);
-        DB::table('subjects')->insert([
-            'title' => 'Literature',
-            'description' => 'Literature'
+        DB::table('classrooms')->insert([
+            'number' => 17,
         ]);
+        DB::table('classrooms')->insert([
+            'number' => 19,
+        ]);
+        DB::table('roles')->insert([
+            'title' => 'Admin',
+            'description' => 'Admin'
+        ]);
+        DB::table('roles')->insert([
+            'title' => 'Teacher',
+            'description' => 'Teacher'
+        ]);
+        DB::table('roles')->insert([
+            'title' => 'Student',
+            'description' => 'Student'
+        ]);
+        DB::table('user_roles')->insert([
+            'user_id' => 1,
+            'role_id' => 2
+        ]);
+        DB::table('user_roles')->insert([
+            'user_id' => 2,
+            'role_id' => 1
+        ]);
+//        DB::table('subjects')->insert([
+//        'title' => 'Math',
+//        'description' => 'Math'
+//    ]);
+//        DB::table('subjects')->insert([
+//            'title' => 'Language',
+//            'description' => 'Language'
+//        ]);
+//        DB::table('subjects')->insert([
+//            'title' => 'History',
+//            'description' => 'History'
+//        ]);
+//        DB::table('subjects')->insert([
+//            'title' => 'Chemistry',
+//            'description' => 'Chemistry'
+//        ]);
+//        DB::table('subjects')->insert([
+//            'title' => 'Geography',
+//            'description' => 'Geographyy'
+//        ]);
+//        DB::table('subjects')->insert([
+//            'title' => 'Literature',
+//            'description' => 'Literature'
+//        ]);
     }
 }
