66d5b2de81e450d7630ad008aa1101b3c41295a1
celebre <komashka.stor@gmail.com>
2016-11-25 21:48:06 +0200

    'delete_unused_date'

--- app/Class_register.php
+++ app/Class_register.php
@@ -7,11 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Class_register extends Model
 {
     public $timestamps = false;
-    public function teacher() {
-
-        return $this->belongsTo('App\Teacher');
-    }
-    public function students() {
-        return $this->belongsToMany('App\Student');
-    }
 }
