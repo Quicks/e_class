b27f0f9292fa46497923b89988a4db4e5a49b598
Julija <you@example.com>
2016-12-01 11:38:04 +0200

    create function select2

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -12,13 +12,17 @@
 
         }
     </style>
-    <select id="test">
-        <option>12</option>
-        <option>42</option>
-        <option>10</option>
-        <option>11</option>
+    <div class="select-test">
+        <select id="test">
+            <option></option>
+            <option value="1">12</option>
+            <option value="2">42</option>
+            <option value="3">10</option>
+            <option value="4">11</option>
 
-    </select>
+        </select>
+    </div>
+
 
     <div class="view hm-black-strong">
         <div class="logo">
@@ -33,6 +37,36 @@
 
             placeholder: "Школы",
             allowClear: true
-        });
+        }).on('change',function (e) {
+           var stabData = [
+               { id: 12, title: '7-a' },
+               { id: 18, title: '10-d' },
+               { id: 25, title: '9-c' }
+           ]
+            renderClassesSelect(stabData)
+            var schoolId = e.target.value
+//            $.ajax({
+//                url: 'classes_by_school',
+//                data: {school_Id:schoolId},
+//                dataType: 'Json',
+//                success: function(data){
+//                    renderClassesSelect(data)
+//                }
+//            })
+        })
+
+        function renderClassesSelect(data){
+            console.log(data)
+             var select_of_classes = $('<select></select>')
+            data.forEach(function(item, i, data) {
+              var option = $('<option></option>')
+                option.val(item.id);
+                option.text(item.title);
+                select_of_classes.append(option);
+            });
+
+           $('.select-test').append(select_of_classes);
+            select_of_classes.select2();
+        }
     })</script>
 @stop
